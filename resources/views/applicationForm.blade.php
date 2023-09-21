33b35d906c1e4a2c982a429d117a926d7dc20698
leor9 <leor91172@gmail.com>
2023-09-21 13:08:38 -0400

    Submit button

--- resources/views/applicationForm.blade.php
+++ resources/views/applicationForm.blade.php
@@ -22,22 +22,37 @@
 
             <form action=""method="post">
                 <label for="first_name">First Name:</label>
+                <br>
                 <input type="text"name="first_name">
+                <br><br>
                 <label for="last_name">Last Name:</label>
+                <br>
                 <input type="text"name="last_name">
+                <br><br>
                 <label for="email">Email:</label>
+                <br>
                 <input type="text"name="email">
+                <br><br>
                 <label for="address">Address:</label>
+                <br>
                 <input type="text"name="address">
+                <br><br>
                 <label for="education">Education:</label>
+                <br>
                 <select name="education">
                     <option value="undergraduate">undergraduate</option>
                     <option value="graduate">graduate</option>
                 </select>
+                <br><br>
                 <label for="years_of_exp">Years of Experience:</label>
+                <br>
                 <input type="text"name="years_of_exp">
+                <br><br>
                 <label for="other">Other:</label>
+                <br>
                 <input type="text"name="Other">
+                <br><br>
+                <button>Submit</button>
             </form>
 
         </div>
