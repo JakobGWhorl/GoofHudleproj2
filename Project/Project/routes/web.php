a2797140e866f7a5a6e877f62312e20782814591
tpob11 <tpobursky21@gmail.com>
2023-09-21 13:13:39 -0400

    added br tages

--- Project/Project/routes/web.php
+++ Project/Project/routes/web.php
@@ -14,8 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('applicationForm');
-});
-Route::get('/showApplicationTable', function () {
-    return view('showApplicationTable');
+    return view('welcome');
 });
