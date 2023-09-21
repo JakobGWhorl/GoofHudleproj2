3998e6b33777cbdc4df02ac395d2fa563def2688
leor9 <leor91172@gmail.com>
2023-09-21 12:37:24 -0400

    added the migration and model.

---
+++ Project/Project/app/Http/Controllers/JobController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Project\App\Models\JobApplication;
+
+class JobController extends Controller
+{
+    
+}
