bcf0e6b2ab2a755f14c899ff18a3c23520c3e6fd
leor9 <leor91172@gmail.com>
2023-09-22 11:39:43 -0400

    idk

--- resources/views/applicationForm.blade.php
+++ resources/views/applicationForm.blade.php
@@ -1,30 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Application Table</title>
-
-        <style type="style/css" href="./css./app.css" rel="stylesheet">
-
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-
-        <!-- Styles -->
-        <style>
-            
-        </style>
     </head>
     <body class="antialiased">
         <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
 
-            <form action="/"method="post">
+            <form action=""method="post">
                 <label for="first_name">First Name:</label>
-                <br><br>
                 <input type="text"name="first_name">
-                <br><br>
                 <label for="last_name">Last Name:</label>
                 <br>
                 <input type="text"name="last_name">
